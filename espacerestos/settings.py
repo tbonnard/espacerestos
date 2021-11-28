74855adca219cbbeb8bbb8bb121e88ba72f3bef4
tbonnard <thibaud.bonnard@gmail.com>
2021-11-27 19:32:11 -0500

    settings

--- espacerestos/settings.py
+++ espacerestos/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-es6&_nlnn967q(==ks0&-bsdw+js=!qjlpgc8c%+(ow7nnyi6s"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
