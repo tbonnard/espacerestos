2e231fc3a41e9e8dcfc68efa29784a31b4961340
tbonnard <thibaud.bonnard@gmail.com>
2021-12-02 17:12:31 -0500

    menu active

--- monespace/static/scripts/nav.js
+++ monespace/static/scripts/nav.js
@@ -21,12 +21,28 @@ document.addEventListener("DOMContentLoaded", function () {
     let now = new Date();
     now.setDate(now.getDate() + numWeeks * 7);
     event_link = document.querySelector('#event_link_date');
-    console.log(event_link);
     event_link_original = event_link.href
     date_to = now.toISOString().split('T')[0];
     event_link.href = event_link_original+date_to;
   }
 
+
+  function check_active_url(i) {
+    console.log(i);
+    let path = window.location.pathname;
+    console.log(path);
+    let menu_path = i.dataset.path;
+    console.log(menu_path);
+
+    if ( path.toLowerCase() == menu_path.toLowerCase()) {
+      i.className = "item_nav item_nav_active";
+    }
+  }
+
+   let side_menu = document.querySelectorAll(".item_nav");
+   side_menu.forEach( i => {
+     check_active_url(i);
+   })
 
 
 });
