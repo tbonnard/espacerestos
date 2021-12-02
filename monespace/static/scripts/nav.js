d5b7a22709a455da66694c049642b4e70ffd54e7
tbonnard <thibaud.bonnard@gmail.com>
2021-12-02 16:59:54 -0500

    sidebar initial

---
+++ monespace/static/scripts/nav.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // check width of the window and based on that hide the menu
+      if (screen.availWidth < 751) {
+          ToggleMenu();
+      }
+
+      // function to hide/unhide the menu-sidebar
+      function ToggleMenu() {
+          document.querySelector('.sidebar').classList.toggle("sidebarTransform");
+      }
+
+    // click on the menu icon toggle the sidebar-menu
+  document.querySelector('.menu_icon').addEventListener('click', function () {
+      ToggleMenu();
+  });
+
+
+  if (document.querySelector('#event_link_date')) {
+    let numWeeks = 8;
+    let now = new Date();
+    now.setDate(now.getDate() + numWeeks * 7);
+    event_link = document.querySelector('#event_link_date');
+    console.log(event_link);
+    event_link_original = event_link.href
+    date_to = now.toISOString().split('T')[0];
+    event_link.href = event_link_original+date_to;
+  }
+
+
+
+});
