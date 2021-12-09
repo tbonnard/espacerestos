3a5b01507a97471f9478ecfa56b4dc0e034018c2
tbonnard <thibaud.bonnard@gmail.com>
2021-12-09 16:47:07 -0500

    js cancel validation location

--- monespace/static/scripts/location_details.js
+++ monespace/static/scripts/location_details.js
@@ -19,4 +19,50 @@ document.addEventListener("DOMContentLoaded", function () {
   })
 
 
+  let buttonCancel = document.querySelectorAll('.button_form_status');
+  let divCancelValidation = document.querySelectorAll('#div_cancel_validation');
+  let trDistrib = document.querySelectorAll('.tr_distrib');
+
+  buttonCancel.forEach(i => {
+    i.addEventListener('click', () => {
+    divCancelValidation.forEach(y => {
+      if (i.dataset.event == y.dataset.event) {
+        y.style.display = 'none';
+        trDistrib.forEach(z => {
+          if (i.dataset.event == z.dataset.event) {
+            let tdValidateCancel = document.createElement('td');
+            tdValidateCancel.className='table_cell';
+            tdValidateCancel.textContent = "Êtes-vous certain?";
+            let divCancel =document.createElement('div');
+            divCancel.style.marginTop = '5px';
+            divCancel.style.marginBottom = '5px';
+            let aValidateCancelYes = document.createElement('a');
+            aValidateCancelYes.href = `${window.location.origin}/event_delete_all/${i.dataset.event}`;
+            aValidateCancelYes.title = `Oui, annuler la distribution`;
+            let iValidateCancelYes = document.createElement('i');
+            iValidateCancelYes.className = "fas fa-check-square";
+            iValidateCancelYes.style.marginRight = '10px';
+            let aValidateCancelNo = document.createElement('a');
+            aValidateCancelNo.title = 'Non, laisser la date';
+            let iValidateCancelNo = document.createElement('i');
+            iValidateCancelNo.className = "far fa-window-close";
+            iValidateCancelNo.style.marginLeft = '5px';
+            z.append(tdValidateCancel);
+            tdValidateCancel.append(divCancel);
+            divCancel.append(aValidateCancelYes)
+            divCancel.append(aValidateCancelNo)
+            aValidateCancelYes.append(iValidateCancelYes);
+            aValidateCancelNo.append(iValidateCancelNo);
+            iValidateCancelNo.addEventListener('click', () => {
+              tdValidateCancel.style.display='none';
+              y.style.display = 'block';
+            })
+          }
+        })
+      }
+    })
+    })
+  })
+
+
 });
